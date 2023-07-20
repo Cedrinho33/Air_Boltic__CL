ebdf872fb687cf7e5b629daafc5d4db27c2573cf
cedriclueck1996@gmail.com <cedriclueck1996@gmail.com>
2023-07-20 18:26:14 +0000

    stg setup

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,13 @@
+orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from dbt-tutorial-raw-data.jaffle_shop.raw_orders
+
+),
+
+SELECT * FROM orders
