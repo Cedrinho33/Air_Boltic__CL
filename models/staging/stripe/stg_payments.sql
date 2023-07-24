f828cf339281f989c8a5b67fc65c06b848690b5b
cedriclueck1996@gmail.com <cedriclueck1996@gmail.com>
2023-07-24 13:43:42 +0000

    push

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,16 @@
+WITH payments AS (
+
+    SELECT
+        id AS payment_id,
+        order_id,
+        payment_method,
+        "accepted" AS status,
+
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 AS amount,
+
+    FROM {{ SOURCE('stripe', 'raw_payments') }}
+
+)
+
+SELECT * FROM payments
